067168cca4a8411c923506809efcec0c76ab031b
siddhu2502 <dsiddharth25@gmail.com>
2023-06-10 11:32:06 +0530

    updated /cmd_vel

--- src/lidar_robot/launch/launch_sim.launch.py
+++ src/lidar_robot/launch/launch_sim.launch.py
@@ -1,35 +1,27 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
-
 from launch import LaunchDescription
+from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch_ros.actions import Node
-
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
 
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
-
-    package_name='lidar_robot' #<--- CHANGE ME
+    package_name = 'lidar_robot'  # <--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -37,11 +29,20 @@ def generate_launch_description():
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    # Launch Rviz2 with fixed frame and TF
+    rviz2 = Node(package='rviz2', executable='rviz2',
+                 arguments=['-d', os.path.join(
+                     get_package_share_directory(package_name), 'config', 'my_rviz_config.rviz'),
+                            'Fixed Frame:=odom'],
+                 output='screen')
 
-
-    # Launch them all!
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        rviz2,
     ])
+
+
+if __name__ == '__main__':
+    generate_launch_description()
